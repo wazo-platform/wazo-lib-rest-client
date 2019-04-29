13035fffde8d8a6506706132aa24ce4a02c2465a
Francois Blackburn <fblackburn@wazo.community>
2019-04-29 14:17:45 -0400

    bump requests from 2.20.0 to 2.21.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from setuptools import setup
 from setuptools import find_packages
 
 REQUIREMENTS = [
-    'requests==2.20.0',
+    'requests==2.21.0',
     'stevedore==1.17.1',
     'six',  # Do not specify version: the last version of setuptools require six>=1.10.0
 ]
